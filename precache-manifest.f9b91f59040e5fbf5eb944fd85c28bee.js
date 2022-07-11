d8ce8dcd51de1244d2105400012afb550a5884cc
luckyhero613 <ag7932613@gmail.com>
2022-07-11 17:32:34 +0530

    Updates

---
+++ precache-manifest.f9b91f59040e5fbf5eb944fd85c28bee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93cbcca0628a9cff33679057b54b01b",
+    "url": "/project/fashby/index.html"
+  },
+  {
+    "revision": "fd36f11ecd2f4c8a801b",
+    "url": "/project/fashby/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c41e378bf65dbdb22e7a",
+    "url": "/project/fashby/static/js/2.a99dd93b.chunk.js"
+  },
+  {
+    "revision": "fd36f11ecd2f4c8a801b",
+    "url": "/project/fashby/static/js/main.56af1e1b.chunk.js"
+  },
+  {
+    "revision": "703e0c248527ee0c06e5",
+    "url": "/project/fashby/static/js/runtime~main.4b1fb7de.js"
+  }
+]);
